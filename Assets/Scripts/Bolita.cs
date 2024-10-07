38739ab4388269a941948f9d605439b374de4e36
alejandrittaa <alejandracortesreyero@gmail.com>
2024-10-07 20:08:41 +0200

    intento de movimiento

--- Assets/Scripts/Bolita.cs
+++ Assets/Scripts/Bolita.cs
@@ -6,10 +6,9 @@ public class Bolita : MonoBehaviour
 {
     Rigidbody bolita;
     [SerializeField] Vector3 direccionF;
-    [SerializeField] int fuerza;
     [SerializeField] Vector3 direccionM;
-    [SerializeField] int fuerzaM;
-    //[SerializeField] Vectro3 direccionS;
+    [SerializeField] int fuerza;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,28 +18,17 @@ public class Bolita : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         //SALTO CON EL ESPACIO
         if(Input.GetKeyDown(KeyCode.Space))
         {
             bolita.AddForce(direccionF * fuerza, ForceMode.Impulse);
         }
         //MOVIMIENTO WASD Y FLECHAS
-        if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) 
-        {
-            bolita.AddForce(direccionM * fuerzaM, ForceMode.Force);
-        }
-        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            bolita.AddForce(-(direccionM) * fuerzaM, ForceMode.Force);
-        }
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            bolita.AddForce(direccionM * fuerzaM, ForceMode.Force);
-        }
-        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            bolita.AddForce(direccionM * fuerzaM, ForceMode.Force);
-        }
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        bolita.AddForce(direccionM * fuerza, ForceMode.Force);
+
 
     }
 }
